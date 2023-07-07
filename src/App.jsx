7691ab789c9566538d8ec9de72f7b2c6f7e1749d
Jose Aguilera <joseaguilera23@gmail.com>
2023-07-07 13:27:45 +0200

    Segunda pre-entrega Jose Aguilera

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import './App.css'
 import { Route,Routes,useLocation } from 'react-router-dom';
 
 import AboutUs from './Pages/About/AboutUs';
-import ItemListContainer from './Pages/Characters/ItemListContainer';
-import Contact from './Pages/Contact/Contact';
-import ItemDetail from './Pages/ItemDetail/ItemDetail';
-import ScrollToTop from './components/ScrollToTop/ScrollToTop';
-import Header from './components/Header/Header';
-import Home from './pages/Home/Home';
-import Footer from './components/Footer/Footer';
+import ItemListContainer from './Pages/Characters/ItemListContainer.jsx';
+import Contact from './Pages/Contact/Contact.jsx';
+import ItemDetail from './Pages/ItemDetail/ItemDetail.jsx';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop.jsx';
+import Header from './components/Header/Header.jsx';
+import Home from './pages/Home/Home.jsx';
+import Footer from './components/Footer/Footer.jsx';
 
 
 
--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     assetsDir: 'assets',
-    rollupOptions: {
-      external: 'NonExistingPath'
-    }
+    
 
   },
 });
