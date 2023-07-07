5673b23365ff3146c8f8de2b941a44c8cc1cf17f
Jose Aguilera <joseaguilera23@gmail.com>
2023-07-07 13:03:59 +0200

    [fix] arreglo de rutas generales

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
     assetsDir: 'assets',
-    sourcemap: true,
+
   },
 });
 
