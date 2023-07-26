b504693da7d4ac0f13e3ef21bdb797dca232d749
Jose Aguilera <joseaguilera23@gmail.com>
2023-07-26 14:50:46 +0200

    [feat ](cart) se crea el componente cart y se agrega cartcontext , es posible agregar articulos al carrito desde la pagina de detalle y contenedor

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ import ScrollToTop from './components/ScrollToTop/ScrollToTop.jsx';
 import Head from './components/Head/Navbar.jsx';
 import Landing from './Pages/Landing/Landing.jsx';
 import Footline from './components/Footline/Footline.jsx';
-import Cart from './Pages/cart/Cart.jsx';
-import Contact from '.Pages/Contact/Contact.jsx'
+import Contact from './Pages/Contact/Contact.jsx';
+import Cart from './Pages/Cart/Cart.jsx';
 
 
 
