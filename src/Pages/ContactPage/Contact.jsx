094e61793d832ac86967dd3b03c6b575391bf687
Jose Aguilera <joseaguilera23@gmail.com>
2023-08-04 12:37:51 +0200

    [feat](contacts) se arregla boton al formulario

--- src/Pages/ContactPage/Contact.jsx
+++ src/Pages/ContactPage/Contact.jsx
@@ -14,10 +14,13 @@ const Contact = () => {
                     <HeadingTitle subTitle1='Discover AI-generated' principalTitle='characters for Virtual' />
                     <p className='font-Inter xs:text-sm lg:text-base md:text-xl'>We are delighted to assist you in finding authentic and meaningful romantic and friendly relationships. Our team is committed to providing you with the best possible service. If you have any questions, comments, or would like to share your experiences, please don`t hesitate to get in touch with us. We value your trust and look forward to hearing from you.</p>
                     </div>
-                <form action="" method="" className="flex flex-col 0 xs:mt-10 lg:mt-0">
+                <form action="" method="" className="flex flex-col items-end 0 xs:mt-10 lg:mt-0">
                     <input type="email" name="email" placeholder="Email" className="  md:w-[33.4375rem] xs:w-[22rem] h-[5.125rem] border-borderCard  rounded-[1.3125rem] bg-bgForm ps-[1.94rem] placeholder-placeHolderColor font-Inter text-sm mb-[1.62rem] customFocus text-placeHolderColor" />
                     <input type="text" placeholder="Subject" className="md:w-[33.4375rem] xs:w-[22rem] h-[5.125rem]  rounded-[1.3125rem]  border-borderCard bg-bgForm ps-[1.94rem] placeholder-placeHolderColor font-Inter text-sm mb-[1.62rem] customFocus text-placeHolderColor" />
                     <textarea name="message" id="" cols="30" rows="10" placeholder="Message" className=" xs:w-[22rem] md:w-[33.4375rem]  border-borderCard lg:w-full rounded-[1.3125rem] bg-bgForm ps-[1.94rem] pt-[1.44rem] placeholder-placeHolderColor font-Inter text-sm text-placeHolderColor customFocus"></textarea>
+                    <button className="border rounded-full w-[9.75rem] h-[2.5625rem] flex items-center justify-center text-placeHolderColor font-Inter text-xs border-borderCard bg-bgForm  mt-[2rem]" type='submit'>
+                        Submit
+                    </button>
                 </form>
             </div>
             <div className='md:flex  flex-col justify-center md:mt-8 items-center text-center mb-72 hidden '>
